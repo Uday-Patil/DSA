f9a2606d27b9b0671f0a0fb20eb61a19425ea427
Uday Patil <Uday Patil>
2017-09-11 01:06:08 +0200

    Moved the code from arr.h header file to ArrDSOperations.c and defined only function pointers in arr.h

--- ArrDSOperations.c
+++ ArrDSOperations.c
@@ -17,7 +17,8 @@
 					we delete data from an array.
  ============================================================================
  */
-
+#include <stdio.h>
+#include <stdlib.h>
 /* function to initialise an array */
 int * initArray(int n) {
 	static int* ptr;
@@ -33,30 +34,49 @@ int * initArray(int n) {
 }
 
 /* function to insert an element at a given in to the passed array */
-int * insertElement(int *p, int element, int size, int pos) {
-	int i;
+int * insertElement(int *p,int size) {
+	int i, pos, ele;
 	
+	back:
+	printf("\n Enter the element to insert: ");
+	scanf("%d", &ele);
+	
+	printf("\n Enter the position where element should be inserted: ");
+	scanf("%d", &pos);
+			
 	if(pos <= size) {
 		for(i=size-1; i>= pos-1; i--)
 		{
 			*(p+(i+1)) = *(p+i);
 		}
+	*(p+(pos-1)) = ele;
 	} 
 	else {
 		printf("\n Position out of bound");
+		goto back;
 	}
-	*(p+(pos-1)) = element;
 	return p;
 }
 
 /* function to delete an element from the passed array to the provided index */
-int * deleteElement(int *p, int pos, int size) {
-	int i=pos;
+int * deleteElement(int *p, int size) {
+	int i, pos;
 	
-	while( i<size) {
-		*(p+(i-1)) = *(p+i);
-		printf("\n %d at %d", *(p+(i-1)), i);
-		i++;
+	again:
+	printf("\n Enter the position at which element should be deleted: ");
+	scanf("%d", &pos);
+	
+	if(pos < size){
+		i = pos;
+	
+		while( i<size) {
+			*(p+(i-1)) = *(p+i);
+			printf("\n %d at %d", *(p+(i-1)), i);
+			i++;
+		}
+	} else {
+		printf("\n Invalid position, please enter a valid position");
+		goto again;
 	}
 	
 	return p;
--- arr.h
+++ arr.h
@@ -12,57 +12,10 @@
  */
 #include <stdio.h>
 /* function to initialise an array */
-int * initArray(int n) {
-	static int* ptr;
-	int i;
-	
-	ptr = (int*)calloc(n,sizeof(int));
-	
-	for ( i = 0; i < n; ++i) {
-		scanf("%d", ptr+i);
-	}
-	
-	return (ptr);
-}
+int * initArray(int n);
 
 /* function to insert an element at a given in to the passed array */
-int * insertElement(int *p,int size) {
-	int i, pos, ele;
-	
-	back:
-	printf("\n Enter the element to insert: ");
-	scanf("%d", &ele);
-	
-	printf("\n Enter the position where element should be inserted: ");
-	scanf("%d", &pos);
-			
-	if(pos <= size) {
-		for(i=size-1; i>= pos-1; i--)
-		{
-			*(p+(i+1)) = *(p+i);
-		}
-	*(p+(pos-1)) = ele;
-	} 
-	else {
-		printf("\n Position out of bound");
-		goto back;
-	}
-	return p;
-}
+int * insertElement(int *p,int size);
 
 /* function to delete an element from the passed array to the provided index */
-int * deleteElement(int *p, int size) {
-	int i, pos;
-	
-	printf("\n Enter the position at which element should be deleted: ");
-	scanf("%d", &pos);
-	
-	i = pos;
-	while( i<size) {
-		*(p+(i-1)) = *(p+i);
-		printf("\n %d at %d", *(p+(i-1)), i);
-		i++;
-	}
-	
-	return p;
-}
+int * deleteElement(int *p, int size);
